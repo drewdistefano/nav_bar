24a0e7c31a579aa83e4eda29f17ebbf87c1d8d9f
Drew <drew.distefano@me.com>
2019-04-10 17:15:36 -0500

    set up boilerplate

--- server/database/db_config.js
+++ server/database/db_config.js
@@ -1,8 +1,19 @@
 const { Client } = require('pg')
 const client = new Client({
-  database: 'inventory'
+  host: 'targit-cart.cttz70y892d2.us-east-2.rds.amazonaws.com',
+  port: 5432,
+  user: 'drewdistefano',
+  database: 'postgres',
+  password: 'password'
 })
 
-client.connect();
+client.connect((err, data)=>{
+  if (err){
+    console.log('error connecting to db:', err)
+  }
+  else {
+    console.log('success, connected to db!')
+  }
+});
 
 module.exports = { client };
